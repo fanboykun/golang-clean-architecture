8596282e16b786773a716353a334ec269afae843
fanboykun <irfanramadhan1812@gmail.com>
2025-11-06 10:04:29 +0700

    fix

--- internal/delivery/http/middleware/auth_middleware.go
+++ internal/delivery/http/middleware/auth_middleware.go
@@ -9,7 +9,11 @@ import (
 
 func NewAuth(userUserCase *usecase.UserUseCase) fiber.Handler {
 	return func(ctx *fiber.Ctx) error {
-		request := &model.VerifyUserRequest{Token: ctx.Get("Authorization", "NOT_FOUND")}
+		token, err := SplitBearerToken(ctx.Get("Authorization", "NOT_FOUND"))
+		if err != nil {
+			return err
+		}
+		request := &model.VerifyUserRequest{Token: token}
 		userUserCase.Log.Debugf("Authorization : %s", request.Token)
 
 		auth, err := userUserCase.Verify(ctx.UserContext(), request)
@@ -26,4 +30,15 @@ func NewAuth(userUserCase *usecase.UserUseCase) fiber.Handler {
 
 func GetUser(ctx *fiber.Ctx) *model.Auth {
 	return ctx.Locals("auth").(*model.Auth)
+}
+
+func SplitBearerToken(token string) (string, error) {
+	if token == "NOT_FOUND" {
+		return "", fiber.ErrUnauthorized
+	}
+	if len(token) < 7 || token[:7] != "Bearer " {
+		return "", fiber.ErrUnauthorized
+	}
+	splitted := token[7:]
+	return splitted, nil
 }
